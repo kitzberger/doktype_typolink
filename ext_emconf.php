c8a5b7b498f78c5c16bcabe65452ae88d736e35d
Philipp Kitzberger <coding@kitze.net>
2023-11-12 10:22:32 +0100

    Bring back ext_emconf.php (for TER)

---
+++ ext_emconf.php
@@ -0,0 +1,22 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Doktype Link => Typolink',
+    'description' => 'Enhance doktype 3 ("Link to External URL") to support typolinks',
+    'category' => 'system',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'author' => 'Philipp Kitzberger',
+    'author_email' => 'typo3@kitze.net',
+    'author_company' => '',
+    'version' => '2.0.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0-11.5.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
