f3ed8792cb5c2ddeef2e1e0e79d989c38e681fdd
Philipp Kitzberger <coding@kitze.net>
2026-05-13 11:35:33 +0200

    Drop support for TYPO3 12

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,10 +11,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Philipp Kitzberger',
     'author_email' => 'typo3@kitze.net',
     'author_company' => '',
-    'version' => '3.0.2',
+    'version' => '3.0.3',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.4.99',
+            'typo3' => '13.4.0-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
